52dbf5e6ae8ffff650e9fcebcc4bd3e28f935822
Alberto Verna <alberto.verna@hotmail.com>
2022-06-25 23:44:11 +0200

    Grammar fix

--- src/event_handlers/successfulPayment.ts
+++ src/event_handlers/successfulPayment.ts
@@ -6,7 +6,7 @@ export const handler: MessageHandler = (bot) => {
         const invoice_payload = JSON.parse(ctx.message.successful_payment.invoice_payload);
         const pdf_link = await getFullNotesLink(invoice_payload.course);
         await ctx.replyWithDocument(pdf_link, {
-            caption: `Grazie per l'acquisto! Ecco il file PDF contenente gli appunti di ${invoice_payload.course}. Per segnalare eventuali errori non esitare a contattare @sAlb98 su Telegram.`
+            caption: `Grazie per l'acquisto! Ecco il file PDF con gli appunti di ${invoice_payload.course}. Per segnalare eventuali errori non esitare a contattare @sAlb98 su Telegram.`
         });
     })
 }
