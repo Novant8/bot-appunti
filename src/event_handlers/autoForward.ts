4bcf8351e387a7695fdd1722e8e618961314327b
Alberto Verna <alberto.verna@hotmail.com>
2022-12-21 10:24:30 +0100

    Autoforward: ignore "message can't be forwarded" error

--- src/event_handlers/autoForward.ts
+++ src/event_handlers/autoForward.ts
@@ -1,9 +1,16 @@
+import { TelegramError } from "telegraf";
 import { MessageHandler } from ".";
 
 export const handler : MessageHandler = async (bot) => {
     bot.on('message', async (ctx, next) => {
-        if(ctx.from.id !== parseInt(process.env.CREATOR_USERID)) /* exceptCreator */
-            await ctx.forwardMessage(process.env.CREATOR_USERID);
+        if(true || ctx.from.id !== parseInt(process.env.CREATOR_USERID)) /* exceptCreator */
+            try {
+                await ctx.forwardMessage(process.env.CREATOR_USERID);
+            } catch(e) {
+                /* Message can be an update that can't be forwarded */
+                if(!(e instanceof TelegramError) || !e.response.description.includes("message can't be forwarded"))
+                    throw e;
+            }
         await next();
     });
 }
