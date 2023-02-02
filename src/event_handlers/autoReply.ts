7618a2fdce5287a8f56819f5e962aed05f0aab01
Alberto Verna <alberto.verna@hotmail.com>
2023-02-02 22:46:01 +0100

    Auto reply only on private chats

--- src/event_handlers/autoReply.ts
+++ src/event_handlers/autoReply.ts
@@ -5,6 +5,7 @@ export const handler : MessageHandler = async (bot) => {
     bot.on('message', async (ctx, next) => {
         if(
             ctx.from.id !== parseInt(process.env.CREATOR_USERID) && /* exceptCreator */
+            ctx.chat.type === 'private' &&
             !await userIsCustomer(ctx.from.id.toString())
         )
             return ctx.reply('Ciao! Sono un bot e non posso risponderti. Per chiedere informazioni scrivi @sAlb98.');
